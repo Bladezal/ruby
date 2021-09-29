33f8a9108d135acf2c8704486e9444565fa5ac07
Daniel Cazal <bladezal@gmail.com>
2021-09-29 01:01:29 -0400

    Tournament with Rakefile and CSV file.

--- main_tournament.rb
+++ main_tournament.rb
@@ -1,16 +1,17 @@
 require 'tournament'
+require 'csv'
 
 teams = ['A', 'B', 'C', 'D']
 tournament = Tournament::Tournament.new()
 
 teams.each {|t| tournament.add_team(t)}
 
-tournament.add_match(tournament.teams[0],tournament.teams[1],3,1)
-tournament.add_match(tournament.teams[2],tournament.teams[3],0,0)
-tournament.add_match(tournament.teams[0],tournament.teams[2],1,1)
-tournament.add_match(tournament.teams[1],tournament.teams[3],2,3)
-tournament.add_match(tournament.teams[0],tournament.teams[3],2,1)
-tournament.add_match(tournament.teams[1],tournament.teams[2],3,1)
+matches = CSV.parse(File.read("csv/matches.csv"), headers: true, converters: :numeric)
+
+matches.each {|m| tournament.add_match(tournament.teams[tournament.teams.index {|t| t.name == m["team_a"]}],
+                                       tournament.teams[tournament.teams.index {|t| t.name == m["team_b"]}],
+                                       m["goals_team_a"],
+                                       m["goals_team_b"])}
 
 tournament.matches.each {|m| m.calc_points}
 tournament.possition_table
---
+++ rakefile.rb
@@ -0,0 +1,27 @@
+require 'tournament'
+require 'csv'
+
+namespace :exec do
+    task :tournament do
+        ruby "main_tournament.rb"
+    end
+end
+
+namespace :init do
+    task :tournament do
+        teams = ['A', 'B', 'C', 'D']
+        tournament = Tournament::Tournament.new()
+
+        teams.each {|t| tournament.add_team(t)}
+
+        matches = CSV.parse(File.read("csv/matches.csv"), headers: true, converters: :numeric)
+
+        matches.each {|m| tournament.add_match(tournament.teams[tournament.teams.index {|t| t.name == m["team_a"]}],
+                                            tournament.teams[tournament.teams.index {|t| t.name == m["team_b"]}],
+                                            m["goals_team_a"],
+                                            m["goals_team_b"])}
+
+        tournament.matches.each {|m| m.calc_points}
+        tournament.possition_table
+    end
+end
